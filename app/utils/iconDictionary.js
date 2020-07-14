ae8afa8984c7c25bf369e7d1d80b873d3950a681
Billy Cole <cole.billys@gmail.com>
2020-07-14 08:21:15 -0700

    add new icons

--- app/utils/iconDictionary.js
+++ app/utils/iconDictionary.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
 import Call from './svg/Call';
+import Calendar from './svg/Calendar';
+import Coin from './svg/Coin';
 import Folder from './svg/Folder';
 import Mail from './svg/Mail';
 import Money from './svg/Money';
+import Note from './svg/Note';
 import Profile from './svg/Profile';
 import Warning from './svg/Warning';
 
 export default (name, size) => {
   const icons = {
     call: Call,
+    calendar: Calendar,
+    coin: Coin,
     folder: Folder,
     mail: Mail,
     money: Money,
+    note: Note,
     profile: Profile,
     warning: Warning,
   };
