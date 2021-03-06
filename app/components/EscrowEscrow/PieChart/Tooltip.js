da57f78c41bc78acb8e7c87c9deade818260212c
Billy Cole <cole.billys@gmail.com>
2021-03-06 11:14:39 -0800

    adjust tooltip position

--- app/components/EscrowEscrow/PieChart/Tooltip.js
+++ app/components/EscrowEscrow/PieChart/Tooltip.js
@@ -22,7 +22,7 @@ const Tooltip = ({ active, datum: { tooltipValue, x }, tooltipPlacement }) => (
     }
     TransitionProps={{ timeout: 0 }}
   >
-    <svg x={tooltipPlacement === 'left' ? 24 : 172} y={98} />
+    <svg x={tooltipPlacement === 'left' ? 25 : 175} y={100} />
   </StyledTooltip>
 );
 
