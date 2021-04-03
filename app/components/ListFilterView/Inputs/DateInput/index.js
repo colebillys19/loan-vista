39ca7f2992339f8f81e1eb23014d3f21045e2d9b
Billy Cole <cole.billys@gmail.com>
2021-04-03 09:24:26 -0700

    fix tooltip jump

--- app/components/ListFilterView/Inputs/DateInput/index.js
+++ app/components/ListFilterView/Inputs/DateInput/index.js
@@ -3,22 +3,39 @@
  * @description ...
  */
 
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import T from 'prop-types';
+
+import { usePrevious } from 'utils/customHooks';
 
 import ForwardRefPicker from './ForwardRefPicker';
 import Tooltip from './Tooltip';
 
-const DateInput = ({ error, onChange, value }) => (
-  <Tooltip
-    open={!!error}
-    placement="top"
-    title={error || ''}
-    TransitionProps={{ timeout: 0 }}
-  >
-    <ForwardRefPicker isError={!!error} onChange={onChange} value={value} />
-  </Tooltip>
-);
+const DateInput = ({ error, onChange, value }) => {
+  const [open, setOpen] = useState(false);
+
+  const prevError = usePrevious(error);
+
+  useLayoutEffect(() => {
+    if (prevError !== '' && error !== prevError) {
+      setOpen(false);
+      setOpen(true);
+    } else {
+      setOpen(!!error);
+    }
+  }, [error, prevError]);
+
+  return (
+    <Tooltip
+      open={open}
+      placement="top"
+      title={error}
+      TransitionProps={{ timeout: 0 }}
+    >
+      <ForwardRefPicker isError={!!error} onChange={onChange} value={value} />
+    </Tooltip>
+  );
+};
 
 DateInput.propTypes = {
   error: T.string.isRequired,
