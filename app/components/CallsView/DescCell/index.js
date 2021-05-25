0e29952675e50a60b797d45be78a3b21ca2690d2
Billy Cole <cole.billys@gmail.com>
2021-05-24 20:11:51 -0700

    implement polling pattern

--- app/components/CallsView/DescCell/index.js
+++ app/components/CallsView/DescCell/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import T from 'prop-types';
 
 import ConditionalRender from 'components/_base-ui/ConditionalRender';
 
 import ExpandableCell from './ExpandableCell';
-import useCheckHeight from './useCheckHeight';
 import { DescTableData } from './styledComponents';
 
 const YeetCell = ({ children }) => {
-  const cellRef = React.useRef(null);
-  const [renderExpandable, setRenderExpandable] = React.useState(false);
-  useCheckHeight(cellRef, setRenderExpandable);
+  const cellRef = useRef(null);
+  const [renderExpandable, setRenderExpandable] = useState(false);
+
+  useLayoutEffect(() => {
+    const {
+      current: { clientHeight },
+    } = cellRef;
+    if (clientHeight > 30) {
+      setRenderExpandable(true);
+    }
+  }, [cellRef]);
 
   return (
     <ConditionalRender
